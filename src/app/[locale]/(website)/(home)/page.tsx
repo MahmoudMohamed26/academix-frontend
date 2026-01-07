fa2fbd84d4ca191196359edf59a6b3351629abd8
MahmoudMohamed26 <gamesforyou26300@gmail.com>
2026-01-07 17:58:29 +0200

    Fix: Source map issue from turbo pack by update nextjs, Generate: Home meta data

--- src/app/[locale]/(website)/(home)/page.tsx
+++ src/app/[locale]/(website)/(home)/page.tsx
@@ -1,7 +1,49 @@
-import SwipperSlider from "./_components/swiper";
+import SwipperSlider from "./_components/swiper"
+import type { Metadata } from "next"
 
-export default function home(){
-  return(
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL!
+
+export async function generateMetadata({ params }: {params: Promise<{locale: string}>}): Promise<Metadata> {
+  const { locale } = await params
+  const isArabic = locale === "ar"
+
+  return {
+    title: isArabic
+      ? "أكاديمكس | تعلم المهارات من خبراء المجال"
+      : "Academix | Learn Skills from Expert Instructors",
+
+    description: isArabic
+      ? "أكاديمكس منصة تعليمية حديثة تقدم دورات احترافية."
+      : "Academix is a modern learning platform offering expert-led courses.",
+
+    openGraph: {
+      url: `${siteUrl}/${locale}`,
+      siteName: "Academix",
+      images: [
+        {
+          url: `${siteUrl}/og/home.png`,
+          width: 1200,
+          height: 630,
+          alt: "Academix LMS Platform",
+        },
+      ],
+      locale: isArabic ? "ar_AR" : "en_US",
+      type: "website",
+    },
+
+    alternates: {
+      canonical: `${siteUrl}/${locale}`,
+      languages: {
+        en: `${siteUrl}/en`,
+        ar: `${siteUrl}/ar`,
+      },
+    },
+  }
+}
+
+export default function home() {
+  console.log(siteUrl)
+  return (
     <>
       <section className="container">
         <SwipperSlider />
